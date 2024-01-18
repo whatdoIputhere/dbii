8f9aa835cb31a37e71ac5d4dd855d76529c4ab38
Carmo, Pedro Miguel <pecarmo@deloitte.pt>
2024-01-18 15:33:33 +0000

    initial db tables and data

---
+++ db_Objects/createtables.sql
@@ -0,0 +1,175 @@
+DROP TABLE IF EXISTS MaoObra CASCADE;
+DROP TABLE IF EXISTS EntregaEncomendaEquipamento CASCADE;
+DROP TABLE IF EXISTS EntregaEncomendaComponente CASCADE;
+DROP TABLE IF EXISTS EncomendaEquipamento CASCADE;
+DROP TABLE IF EXISTS EncomendaComponente CASCADE;
+DROP TABLE IF EXISTS EstadoEntrega CASCADE;
+DROP TABLE IF EXISTS EstadoEncomenda CASCADE;
+DROP TABLE IF EXISTS Fornecedor CASCADE;
+DROP TABLE IF EXISTS ProducaoEquipamento CASCADE;
+DROP TABLE IF EXISTS DetalhesFatura CASCADE;
+DROP TABLE IF EXISTS Fatura CASCADE;
+DROP TABLE IF EXISTS Equipamento CASCADE;
+DROP TABLE IF EXISTS TipoEquipamento CASCADE;
+DROP TABLE IF EXISTS ComponenteArmazem CASCADE;
+DROP TABLE IF EXISTS Componente CASCADE;
+DROP TABLE IF EXISTS Armazem CASCADE;
+DROP TABLE IF EXISTS Utilizador CASCADE;
+DROP TABLE IF EXISTS TipoUtilizador CASCADE;
+
+CREATE TABLE TipoUtilizador(
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE Utilizador(
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    palavraPasse varchar(255) NOT NULL,
+    tipo int NOT NULL REFERENCES TipoUtilizador(id),
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE Armazem(
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    descricao varchar(255) NOT NULL,
+    criadoPor int NOT NULL REFERENCES Utilizador(id),
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE Componente(
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    descricao varchar(255) NOT NULL,
+    preco float NOT NULL,
+    iva int NOT NULL,
+    imagem bytea NOT NULL,
+    criadoPor int NOT NULL REFERENCES Utilizador(id),
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE ComponenteArmazem(
+    componente int REFERENCES Componente(id),
+    armazem int REFERENCES Armazem(id),
+    quantidade int NOT NULL,
+    CONSTRAINT pk_ComponenteArmazem PRIMARY KEY (componente, armazem),
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE TipoEquipamento(
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    criadoPor int NOT NULL REFERENCES Utilizador(id),
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE Equipamento (
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    descricao varchar(255) NOT NULL,
+    tipo int NOT NULL REFERENCES TipoEquipamento(id),
+    preco float NOT NULL,
+    iva int NOT NULL,
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    criadoPor int NOT NULL REFERENCES Utilizador(id),
+    imagem bytea NOT NULL,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE Fatura(
+    id serial PRIMARY KEY,
+    numero int NOT NULL,
+    data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE DetalhesFatura(
+    fatura int REFERENCES Fatura(id),
+    componente int REFERENCES Componente(id),
+    quantidade int NOT NULL,
+    preco float NOT NULL,
+    iva int NOT NULL,
+    CONSTRAINT pk_DetalhesFatura PRIMARY KEY (fatura, componente),
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE ProducaoEquipamento(
+    id serial PRIMARY KEY,
+    equipamento int REFERENCES Equipamento(id),
+    componente int REFERENCES Componente(id),
+    criadoPor int NOT NULL REFERENCES Utilizador(id),
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE Fornecedor(
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    morada varchar(255) NOT NULL,
+    telefone varchar(12) NOT NULL,
+    email varchar(255) NOT NULL,
+    nif varchar(9) NOT NULL,
+    criadoPor int NOT NULL REFERENCES Utilizador(id),
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE EstadoEncomenda(
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE EstadoEntrega(
+    id serial PRIMARY KEY,
+    nome varchar(255) NOT NULL,
+    isEnabled boolean DEFAULT true
+);
+
+CREATE TABLE EncomendaComponente(
+    id serial PRIMARY KEY,
+    fornecedor int REFERENCES Fornecedor(id),
+    isEnabled boolean DEFAULT true,
+    estado int REFERENCES EstadoEncomenda(id),
+    criadoPor int NOT NULL REFERENCES Utilizador(id),
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE EncomendaEquipamento(
+    id serial PRIMARY KEY,
+    utilizador int REFERENCES Utilizador(id),
+    estado int references EstadoEncomenda(id),
+    criadoPor int NOT NULL REFERENCES Utilizador(id),
+    criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    dataEncomenda TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE EntregaEncomendaComponente(
+    id serial PRIMARY KEY,
+    encomenda int REFERENCES EncomendaComponente(id),
+    componente int REFERENCES Componente(id),
+    estado int REFERENCES EstadoEntrega(id),
+    dataEntrega TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE EntregaEncomendaEquipamento(
+    id serial PRIMARY KEY,
+    encomenda int REFERENCES EncomendaEquipamento(id),
+    equipamento int REFERENCES Equipamento(id),
+    estado int REFERENCES EstadoEntrega(id),
+    dataEntrega TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE MaoObra(
+    id serial PRIMARY KEY,
+    tipo varchar(255) NOT NULL,
+    descricao varchar(255) NOT NULL,
+    preco float NOT NULL
+);
---
+++ db_Objects/populatedata.sql
@@ -0,0 +1,115 @@
+INSERT INTO TipoUtilizador (nome)
+VALUES ('Administrador'),
+       ('Cliente');
+
+SELECT * FROM Tipoutilizador;
+
+INSERT INTO Utilizador (nome, email, palavraPasse, tipo)
+VALUES ('Administrador', 'admin@gmail.com', 'admin', 1),
+       ('Cliente 1', 'user@gmail.com', 'user', 2);
+
+SELECT * FROM Utilizador;
+
+INSERT INTO Armazem (nome, descricao,criadoPor)
+VALUES ('Armazém 1', 'Armazém 1', 1),
+       ('Armazém 2', 'Armazém 2', 1);
+
+SELECT * FROM Armazem;
+
+INSERT INTO Componente (nome, descricao, preco, iva,imagem,criadoPor)
+VALUES ('Intel i7-9700k', 'Processador Intel Core i7-9700K 3.6GHz c/ Turbo 4.9GHz 12MB Skt1151', 399.90, 23,'',1),
+       ('Placa Gráfica RTX 2080 Ti', 'Placa Gráfica MSI GeForce RTX 2080 Ti Gaming X Trio 11GB GDDR6', 1499.90, 23, '', 1),
+       ('MSI Z390 GAMING', 'Motherboard MSI MPG Z390 GAMING EDGE AC', 199.90, 23, '', 1),
+       ('Corsair Vengeance 16GB', 'Memória RAM Corsair Vengeance RGB PRO 16GB (2x8GB) DDR4 3200MHz C16 Pretas', 129.90, 23, '', 1),
+       ('Samsung 970 Evo Plus', 'Disco Rígido SSD Samsung 970 Evo Plus 500GB M.2 NVMe', 129.90, 23, '', 1),
+       ('Corsair RM750x 750W', 'Fonte de Alimentação Corsair RM750x 750W 80 Plus Gold Modular', 129.90, 23, '', 1),
+       ('NZXT H500 Preta', 'Caixa NZXT H500 Preta', 79.90, 23, '', 1),
+       ('Corsair LL120', 'Ventoinha Corsair LL120 RGB LED 120mm PWM', 29.90, 23, '', 1),
+       ('Monitor 24" LED - LG 24MK400H-B', 'Monitor 24" LED - LG 24MK400H-B', 99.90, 23, '', 1),
+       ('Teclado Mecânico - Corsair K70 RGB MK.2', 'Teclado Mecânico - Corsair K70 RGB MK.2', 159.90, 23, '', 1),
+       ('Rato Gaming - Corsair M65 RGB Elite', 'Rato Gaming - Corsair M65 RGB Elite', 69.90, 23, '', 1),
+       ('Webcam - Logitech C920 HD Pro', 'Webcam - Logitech C920 HD Pro', 99.90, 23, '', 1);
+
+SELECT * FROM Componente;
+
+INSERT INTO ComponenteArmazem (componente, armazem, quantidade)
+VALUES (1, 1, 10),
+       (2, 1, 10),
+       (3, 1, 10),
+       (4, 2, 10),
+       (5, 2, 10),
+       (6, 2, 10),
+       (7, 1, 10),
+       (8, 1, 10),
+       (9, 1, 10),
+       (10, 2, 10),
+       (11, 1, 10),
+       (12, 1, 10);
+
+SELECT * FROM ComponenteArmazem;
+
+INSERT INTO TipoEquipamento (nome,criadoPor)
+VALUES ('Télemovel', 1),
+       ('Computador',1),
+       ('Tablet',1),
+       ('Portátil',1),
+       ('Consola',1),
+       ('Impressora',1),
+       ('Monitor',1),
+       ('Teclado',1),
+       ('Rato',1),
+       ('Webcam',1);
+
+SELECT * FROM TipoEquipamento;
+
+INSERT INTO Equipamento (nome, descricao, tipo, preco, iva, imagem, criadoPor)
+VALUES ('PC Gaming #1', 'Processador: i7-9700k, Placa Gráfica: RTX 2080 Ti, Memória RAM: 16GB, Disco Rígido: 500GB, Fonte de Alimentação: 750W, Caixa: NZXT H500 Preta, Ventoinha: Corsair LL120', 2, 2249.99, 23, '', 1),
+       ('Monitor LG 24"', 'Monitor 24" LED - LG 24MK400H-B', 7, 99.90, 23, '', 1),
+       ('Corsair K70', 'Teclado Mecânico - Corsair K70 RGB MK.2', 8, 159.90, 23, '', 1),
+       ('Corsair m65', 'Rato Gaming - Corsair M65 RGB Elite', 9, 69.90, 23, '', 1),
+       ('Logitech C920', 'Webcam - Logitech C920 HD Pro', 10, 99.90, 23, '', 1);
+
+SELECT * FROM Equipamento;
+
+INSERT INTO ProducaoEquipamento (equipamento, componente,criadoPor)
+VALUES (1, 1,1),
+       (1, 2,1),
+       (1, 3,1),
+       (1, 4,1),
+       (1, 5,1),
+       (1, 6,1),
+       (1, 7,1),
+       (1, 8,1);
+
+SELECT * FROM ProducaoEquipamento;
+
+INSERT INTO Fornecedor (nome, morada, telefone, email, nif,criadoPor)
+VALUES ('Fornecedor 1', 'Rua do Fornecedor 1', '912345678', 'fornecedor1@gmail.com', '123456789',1),
+       ('Fornecedor 2', 'Rua do Fornecedor 2', '912345678', 'fornecedor2@gmail.com', '123456789',1);
+
+SELECT * FROM Fornecedor;
+
+INSERT INTO EstadoEncomenda (nome)
+VALUES ('Em processamento'),
+       ('Em transporte'),
+       ('Parcialmente Entregue'),
+       ('Entregue');
+
+SELECT * FROM EstadoEncomenda;
+
+INSERT INTO EstadoEntrega (nome)
+VALUES ('Em processamento'),
+       ('Em transporte'),
+       ('Entregue');
+
+SELECT * FROM EstadoEntrega;
+
+INSERT INTO MaoObra (tipo, descricao, preco)
+VALUES  ('Montagem (curta duração)', 'Montagem com duração inferior a 30 minutos' ,20),
+        ('Montagem (média duração)', 'Montagem com duração entre 30 minutos a 2 horas',30),
+        ('Montagem (longa duração)', 'Montagem com duração superior a 2 horas',50),
+        ('Reparação (curta duração)', 'Reparação com duração inferior a 30 minutos',10),
+        ('Reparação (média duração)', 'Reparação com duração entre 30 minutos e 2 horas',30),
+        ('Reparação (longa duração)', 'Reparação com duração superior a 2 horas',50);
+
+ SELECT * FROM MaoObra;
